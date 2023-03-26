f4acd1fb1465fbe0eff9add4c4aef58cef1989d2
Louis Beaumont <louis.beaumont@gmail.com>
2023-03-26 12:09:08 +0000

    chore: sync embedbase 0.9.1

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ghcr.io/different-ai/embedbase:0.9.0-minimal
+FROM ghcr.io/different-ai/embedbase:0.9.1-minimal
 COPY requirements.txt requirements.txt
 RUN apt-get update && apt-get install -y git && apt-get clean && \
     pip install -r requirements.txt && rm requirements.txt
--- main.py
+++ main.py
@@ -1,14 +1,26 @@
+import os
 from embedbase import get_app
-
-from embedbase.settings import get_settings
+from embedbase.settings import get_settings_from_file
 from fastapi.middleware.cors import CORSMiddleware
 from middlewares.auth_api_key.auth_api_key import AuthApiKey
-from embedbase.supabase_db import Supabase
+from embedbase.database.supabase_db import Supabase
+from embedbase.embedding.openai import OpenAI
 
-settings = get_settings()
+config_path = "config.yaml"
+SECRET_PATH = "/secrets" if os.path.exists("/secrets") else ".."
+# if can't find config.yaml in .. try . now (local dev)
+if not os.path.exists(os.path.join(SECRET_PATH, config_path)):
+    SECRET_PATH = "."
+
+if not os.path.exists(os.path.join(SECRET_PATH, config_path)):
+    # exit process with error
+    print(f"ERROR: Missing {config_path} file")
+
+settings = get_settings_from_file(os.path.join(SECRET_PATH, config_path))
 
 app = (
     get_app(settings)
+    .use(OpenAI(settings.openai_api_key, settings.openai_organization))
     .use(Supabase(settings.supabase_url, settings.supabase_key))
     .use(AuthApiKey)
     .use(
